cc71c20ec34acf5d6fd18b4e265bd50df53e97c3
Kaku <haoyujievera@gmail.com>
2024-01-23 01:06:46 +1300

    Remove eyebrow blink detection

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ import time
 import math
 
 BLINK_GAP = 2
-DEFAULT_EYE_BLINK_THRESHOLD = 3.4
-DEFAULT_EYEBROW_BLINK_THRESHOLD = 2.3
+DEFAULT_EYE_BLINK_THRESHOLD = 3
 
 LEFT_EYE_RATIO_CACHE = []
 RIGHT_EYE_RATIO_CACHE = []
-LEFT_EYEBROW_RATIO_CACHE = []
-RIGHT_EYEBROW_RATIO_CACHE = []
 LAST_BLINK = []
 SHOW_EMOJI = []
 
-# 初始化
+# Initialization
 # cap = cv2.VideoCapture("test1.MOV")
 # cap = cv2.VideoCapture("test2.MOV")
 # cap = cv2.VideoCapture("test3.MOV")
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=3)
 
@@ -31,6 +28,7 @@ def euclidean_distance(point, point1):
     distance = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
     return distance
 
+# Display winky emoji on nose tip
 def display_emoji(frame, landmark):
     emoji_img = cv2.imread("winky.png", cv2.IMREAD_UNCHANGED)
     emoji_resized = cv2.resize(
@@ -56,6 +54,7 @@ def display_emoji(frame, landmark):
         )
     return frame
 
+# Draw heartbeat graph of blink ratio
 def draw_heartbeat(data, width, height):
     graph = np.zeros((height, width, 3), dtype=np.uint8)
 
@@ -121,7 +120,7 @@ def eye_blink_detection(idx, landmarks):
     reRatio = rhDistance / rvDistance
     leRatio = lhDistance / lvDistance
 
-    # calculate personalized eye ratio
+    # Real-time tracking of eye ratio changes
     if len(LEFT_EYE_RATIO_CACHE) < idx + 1:
         LEFT_EYE_RATIO_CACHE.append([])
     if len(RIGHT_EYE_RATIO_CACHE) < idx + 1:
@@ -134,7 +133,7 @@ def eye_blink_detection(idx, landmarks):
         RIGHT_EYE_RATIO_CACHE[idx].pop(0)
     RIGHT_EYE_RATIO_CACHE[idx].append(reRatio)
     
-    # calculate personalized blink threshold
+    # Calculate personalized blink threshold
     average_left_ratio = sum(LEFT_EYE_RATIO_CACHE[idx][-20:])/20
     average_right_ratio = sum(RIGHT_EYE_RATIO_CACHE[idx][-20:])/20
     if len(LEFT_EYE_RATIO_CACHE[idx]) == 100 and len(RIGHT_EYE_RATIO_CACHE[idx]) == 100:
@@ -143,85 +142,6 @@ def eye_blink_detection(idx, landmarks):
         threshold = DEFAULT_EYE_BLINK_THRESHOLD
 
     return threshold, leRatio, reRatio
-
-
-# Indices for right and left eyes
-right_indices = [33, 133, 159, 145]
-left_indices = [362, 263, 386, 374]
-
-
-# 新的眨眼检测函数
-def eyebrow_blink_detection(idx, landmarks, height, width):
-    # 右眼坐标点
-    right_eyebrow_left = (
-        int(landmarks[55].x * width),
-        int(landmarks[55].y * height),
-    )  # right eyebrow left
-    right_eyebrow_right = (
-        int(landmarks[46].x * width),
-        int(landmarks[46].y * height),
-    )  # right eyebrow right
-    right_eyebrow = (
-        int(landmarks[65].x * width),
-        int(landmarks[65].y * height),
-    )  # right eyebrow bottom
-    right_eyelid = (
-        int(landmarks[158].x * width),
-        int(landmarks[158].y * height),
-    )  # right eye top
-
-    # 左眼坐标点
-    left_eyebrow_left = (
-        int(landmarks[276].x * width),
-        int(landmarks[276].y * height),
-    )  # left eyebrow left
-    left_eyebrow_right = (
-        int(landmarks[285].x * width),
-        int(landmarks[285].y * height),
-    )  # left eyebrow right
-    left_eyebrow = (
-        int(landmarks[295].x * width),
-        int(landmarks[295].y * height),
-    )  # left eyebrow bottom
-    left_eyelid = (
-        int(landmarks[385].x * width),
-        int(landmarks[385].y * height),
-    )  # left eye top
-
-    # 计算距离
-    right_eyebrow_distance = euclidean_distance(right_eyebrow_left, right_eyebrow_right)
-    right_eye_distance = euclidean_distance(right_eyebrow, right_eyelid)
-    left_eyebrow_distance = euclidean_distance(left_eyebrow_left, left_eyebrow_right)
-    left_eye_distance = euclidean_distance(left_eyebrow, left_eyelid)
-
-    reRatio = right_eyebrow_distance / right_eye_distance
-    leRatio = left_eyebrow_distance / left_eye_distance
-
-    # calculate personalized eyebrow status
-    if len(LEFT_EYEBROW_RATIO_CACHE) < idx + 1:
-        LEFT_EYEBROW_RATIO_CACHE.append([])
-    if len(RIGHT_EYEBROW_RATIO_CACHE) < idx + 1:
-        RIGHT_EYEBROW_RATIO_CACHE.append([])
-
-    if len(LEFT_EYEBROW_RATIO_CACHE[idx]) >= 20:
-        LEFT_EYEBROW_RATIO_CACHE[idx].pop(0)
-    LEFT_EYEBROW_RATIO_CACHE[idx].append(leRatio)
-    if len(RIGHT_EYEBROW_RATIO_CACHE[idx]) >= 20:
-        RIGHT_EYEBROW_RATIO_CACHE[idx].pop(0)
-    RIGHT_EYEBROW_RATIO_CACHE[idx].append(reRatio)
-    
-    average_left_eyebrow = sum(LEFT_EYEBROW_RATIO_CACHE[idx])/len(LEFT_EYEBROW_RATIO_CACHE[idx])
-    average_right_eyebrow = sum(RIGHT_EYEBROW_RATIO_CACHE[idx])/len(RIGHT_EYEBROW_RATIO_CACHE[idx])
-    if len(LEFT_EYEBROW_RATIO_CACHE[idx]) == 20 and len(RIGHT_EYEBROW_RATIO_CACHE[idx]) == 20:
-        threshold = (average_left_eyebrow + average_right_eyebrow) / 2 - 0.1
-    else:
-        threshold = DEFAULT_EYEBROW_BLINK_THRESHOLD
-
-    ratio = (leRatio + reRatio) / 2
-
-    # 返回平均距离
-    return threshold, ratio
-
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -238,9 +158,6 @@ while cap.isOpened():
         for face_landmarks in results.multi_face_landmarks:
             eye_threshold, left_ratio, right_ratio = eye_blink_detection(idx, face_landmarks.landmark)
             eye_ratio = (left_ratio + right_ratio) / 2
-            eyebrow_threshold, eyebrow_ratio = eyebrow_blink_detection(
-                idx, face_landmarks.landmark, height, width
-            )
 
             offset = idx * 300
             cv2.putText(frame, f"Left Ratio: {left_ratio:.2f}", (250, 60 + offset), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2)
@@ -250,7 +167,6 @@ while cap.isOpened():
             frame[0 + offset:100 + offset, 0:200] = left_eye_graph
             frame[100 + offset:200 + offset, 0:200] = right_eye_graph
 
-            # 检查两种方法是否都检测到眨眼
             if len(LAST_BLINK) < idx + 1:
                 LAST_BLINK.append(time.time())
             if len(SHOW_EMOJI) < idx + 1:
@@ -269,9 +185,12 @@ while cap.isOpened():
             if SHOW_EMOJI[idx]:
                 frame = display_emoji(frame, face_landmarks.landmark)
 
-            cv2.putText(frame, f"Since Last Blink: {since_last_blink:.2f}s", (10, 260 + idx * 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2)
+            if len(LEFT_EYE_RATIO_CACHE[idx]) >= 100 or len(RIGHT_EYE_RATIO_CACHE[idx]) >= 100:            
+                cv2.putText(frame, f"Since Last Blink: {since_last_blink:.2f}s", (10, 260 + idx * 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2)
+            else:
+                cv2.putText(frame, f"Since Last Blink: Calibrating...", (10, 260 + idx * 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2)
 
-            # 绘制面部标记
+            # Draw face contours
             # mp.solutions.drawing_utils.draw_landmarks(
             #     frame, face_landmarks, mp_face_mesh.FACEMESH_CONTOURS
             # )
